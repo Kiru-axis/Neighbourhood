1a845c8ba476a2d79638fc1975558522e455981d
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-25 18:48:28 +0300

    all neighboouhoods view function

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from .forms import SignupForm
+from .models import NeighbourHood, Profile, Business, Post
+
 # Create your views here.
 def index(request):
     return render(request, 'hood/index.html')
@@ -18,3 +20,13 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+# hood section
+def hoods(request):
+    all_hoods = NeighbourHood.objects.all()
+    all_hoods = all_hoods[::-1]
+    params = {
+        'all_hoods': all_hoods,
+    }
+    print(all_hoods)
+    return render(request, 'hood/all_hoods.html', params)
