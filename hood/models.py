e5d3718d8c0d27ac099489d0954af32c98d5f976
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-25 18:30:01 +0300

    neughbourhood model class

--- hood/models.py
+++ hood/models.py
@@ -6,3 +6,12 @@ from django.utils import timezone
 
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo = models.ImageField(upload_to='hoods/',default='hood.jpg')
+    description = models.TextField()
+    health_workers = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+    
