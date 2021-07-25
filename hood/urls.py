6514123f2ea3b865178738fb7f52f20c75f81e69
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-25 19:12:41 +0300

    singlehood,leave and join url routes

--- hood/urls.py
+++ hood/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('all-hoods/', views.hoods, name='hood'),
     path('new-hood/', views.create_hood, name='new-hood'),
+    path('single-hood/<id>', views.single_hood, name='single-hood'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
