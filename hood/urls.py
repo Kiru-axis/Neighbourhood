5f9b86e4be9b7e1719ecaf6f7d2ed2859ac1cc06
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-25 18:50:03 +0300

    urls for signup,account and all hoods

--- hood/urls.py
+++ hood/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
