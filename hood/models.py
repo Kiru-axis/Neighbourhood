8d35a5f8a792efdd7ef73b613590f92e9c73b237
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-26 14:36:04 +0300

    cloudinary to handle static and media on deploy

--- hood/models.py
+++ hood/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.utils import timezone
-
+# Create your models here.
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo = models.ImageField(upload_to='hoods/',default='hood.jpg')
+    hood_logo = CloudinaryField('hoods',default='hood.jpg')
     description = models.TextField()
     health_workers = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
@@ -40,8 +41,8 @@ class NeighbourHood(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
-    bio = models.TextField(max_length=254, blank=True)
-    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=254,default="My Bio", blank=True)
+    profile_picture =CloudinaryField('images', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
